98348502158bb484206e01973c7a2e8f41f50ddc
arunaszygis@gmail.com <Kovas2020>
2020-03-17 16:34:34 +0200

    Added templates folder.

---
+++ flask_app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from main import get_coronavirus_data
+
+app = Flask(__name__)
+
+heading = "Main page with button"
+
+@app.route('/')
+def index():
+    return render_template('index.html', heading=heading)
+
+@app.route('/table')
+def return_table():
+    countries = get_coronavirus_data()
+    return render_template('table.html', countries=countries)
+
+if __name__ == '__main__': #it can be run Only directly.
+    app.run(debug = True)
